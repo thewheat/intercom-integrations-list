2afa9acca8ebd1516a827d44bcf1990932850aaf
Timothy Lim <tim.lim@intercom.io>
2018-09-16 22:46:41 +0800

    Add admin path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :integrations
   resources :tags
+  resources :admin
+  root 'integrations#index'
 end
 
 
